91aa67e3ce771dc5e2169c79c58d624f793d7bef
phxswjjj <phxswjjj@gmail.com>
2023-08-05 23:53:54 +0800

    add job monitor

--- QuartzJobManager/Form1.Designer.cs
+++ QuartzJobManager/Form1.Designer.cs
@@ -32,8 +32,10 @@
             this.tslJobStatistic = new System.Windows.Forms.ToolStripStatusLabel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.tsmToggleScheduler = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvJobs = new System.Windows.Forms.DataGridView();
             this.statusStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJobs)).BeginInit();
             this.SuspendLayout();
             // 
             // statusStrip1
@@ -69,11 +71,24 @@
             this.tsmToggleScheduler.Text = "Start";
             this.tsmToggleScheduler.Click += new System.EventHandler(this.tsmToggleScheduler_Click);
             // 
+            // dgvJobs
+            // 
+            this.dgvJobs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvJobs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJobs.Location = new System.Drawing.Point(12, 27);
+            this.dgvJobs.Name = "dgvJobs";
+            this.dgvJobs.RowTemplate.Height = 24;
+            this.dgvJobs.Size = new System.Drawing.Size(776, 398);
+            this.dgvJobs.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvJobs);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -84,6 +99,7 @@
             this.statusStrip1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJobs)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -95,6 +111,7 @@
         private System.Windows.Forms.ToolStripStatusLabel tslJobStatistic;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem tsmToggleScheduler;
+        private System.Windows.Forms.DataGridView dgvJobs;
     }
 }
 
--- QuartzJobManager/Form1.cs
+++ QuartzJobManager/Form1.cs
@@ -22,9 +22,17 @@ namespace QuartzJobManager
         private IScheduler Scheduler;
         private readonly ILogger LogService;
 
+        const string ColNameJobKey = "ColJobKey";
+        const string ColNameJobDescription = "ColJobDescription";
+        const string ColNameJobStatus = "ColJobStatus";
+        const string ColNameJobRunTime = "ColJobRunTime";
+        const string ColNameJobPreviousFiredTime = "ColJobPreviousFiredTime";
+
         public Form1()
         {
             InitializeComponent();
+
+            InitializeJobViewer();
 
             var logger = LogFactory.Create<Form1>();
             this.LogService = logger;
@@ -32,6 +40,159 @@ namespace QuartzJobManager
             InitializeQuartz();
 
             InitializeScanJobStatisticTask();
+
+            InitializeMonitorJobTask();
+        }
+
+        private void InitializeMonitorJobTask()
+        {
+            var runMonitor = new Action(() =>
+            {
+                var logger = LogFactory.Create("MonitorJobTas");
+                logger.Information("{ModuleId} init");
+
+                var scheduler = this.Scheduler;
+                var gv = this.dgvJobs;
+
+                var jobKeys = scheduler.GetJobKeys(GroupMatcher<JobKey>.AnyGroup()).Result;
+                var removeableJobKeys = new List<JobKey>(jobKeys);
+
+                var removeRows = new List<DataGridViewRow>();
+                foreach (DataGridViewRow grow in gv.Rows)
+                {
+                    var job = (IJobDetail)grow.Tag;
+
+                    if (removeableJobKeys.Contains(job.Key))
+                    {
+                        //update job
+                        job = scheduler.GetJobDetail(job.Key).Result;
+                        BindData(grow, job);
+                    }
+                    else
+                    {
+                        //remove job(add list)
+                        removeRows.Add(grow);
+                    }
+                    removeableJobKeys.Remove(job.Key);
+                }
+                //execute remove job
+                foreach (var removeRow in removeRows)
+                    gv.Rows.Remove(removeRow);
+
+                //add job
+                foreach (var jobKey in removeableJobKeys)
+                {
+                    gv.Rows.Add();
+                    int newRowIndex = gv.Rows.GetLastRow(DataGridViewElementStates.None);
+                    var newRow = gv.Rows[newRowIndex];
+                    var job = scheduler.GetJobDetail(jobKey).Result;
+                    BindData(newRow, job);
+                }
+
+                var executingJobs = scheduler.GetCurrentlyExecutingJobs().Result;
+                foreach (DataGridViewRow grow in gv.Rows)
+                {
+                    var job = (IJobDetail)grow.Tag;
+                    var executingJob = executingJobs.FirstOrDefault(j => j.JobDetail.Key == job.Key);
+
+                    var cellStatus = grow.Cells[ColNameJobStatus];
+                    var cellRunTime = grow.Cells[ColNameJobRunTime];
+
+                    if (executingJob != null)
+                    {
+                        cellStatus.Value = "Running";
+                        cellRunTime.Value = executingJob.JobRunTime.ToString(@"hh\:mm\:ss\.fff");
+                    }
+                    else if (scheduler.InStandbyMode)
+                        cellStatus.Value = "Paused";
+                    else
+                    {
+                        cellStatus.Value = "Idle";
+                    }
+                }
+            });
+
+            //隨 Process 生死不特別處理
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    //延遲啟動，否則 Invoke 不好處理
+                    if (!this.InvokeRequired)
+                    {
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+                    try
+                    {
+                        this.Invoke(new MethodInvoker(runMonitor));
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        //ignore: 關閉程式時會拋錯誤
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+
+                    Thread.Sleep(200);
+                }
+            });
+        }
+
+        private void BindData(DataGridViewRow newRow, IJobDetail job)
+        {
+            var gv = newRow.DataGridView;
+            var scheduler = this.Scheduler;
+
+            newRow.Tag = job;
+            newRow.Cells[ColNameJobKey].Value = job.Key.Name;
+            newRow.Cells[ColNameJobDescription].Value = job.Description;
+
+            var triggers = scheduler.GetTriggersOfJob(job.Key).Result;
+            var prevFiredTime = triggers.Max(t => t.GetPreviousFireTimeUtc());
+            var prevFiredTimeText = "NA";
+            if (prevFiredTime.HasValue)
+                prevFiredTimeText = prevFiredTime.Value.LocalDateTime.ToString("yyyy/MM/dd HH:mm:ss");
+            newRow.Cells[ColNameJobPreviousFiredTime].Value = prevFiredTimeText;
+        }
+
+        private void InitializeJobViewer()
+        {
+            var gv = this.dgvJobs;
+
+            gv.AutoGenerateColumns = false;
+            gv.AllowUserToAddRows = false;
+            gv.AllowUserToDeleteRows = false;
+            gv.ReadOnly = true;
+
+            var generateTextColumn = new Func<string, string, DataGridViewTextBoxColumn>((name, title) =>
+            {
+                var col = new DataGridViewTextBoxColumn();
+                col.Name = name;
+                col.HeaderText = title;
+                return col;
+            });
+
+            var colJobName = generateTextColumn(ColNameJobKey, "Job");
+            gv.Columns.Add(colJobName);
+
+            var colJobDescription = generateTextColumn(ColNameJobDescription, "Description");
+            gv.Columns.Add(colJobDescription);
+
+            var colStatus = generateTextColumn(ColNameJobStatus, "Status");
+            colStatus.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            gv.Columns.Add(colStatus);
+
+            var colRunTime = generateTextColumn(ColNameJobRunTime, "Run Time");
+            colRunTime.Width = 80;
+            gv.Columns.Add(colRunTime);
+
+            var colPreviousFiredTime = generateTextColumn(ColNameJobPreviousFiredTime, "Prev Fired Time");
+            colPreviousFiredTime.Width = 120;
+            gv.Columns.Add(colPreviousFiredTime);
         }
 
         private void InitializeScanJobStatisticTask()
@@ -68,6 +229,9 @@ namespace QuartzJobManager
             var job2 = JobBuilder.Create<Job1>()
                 .WithIdentity("Job2")
                 .Build();
+            var job3 = JobBuilder.Create<Job1>()
+                .WithIdentity("Job3")
+                .Build();
 
             var every10s = TriggerBuilder
                 .Create()
@@ -92,9 +256,15 @@ namespace QuartzJobManager
                 .WithCronSchedule("*/30 * * ? * * *")
                 .Build();
 
+            var every2min = TriggerBuilder
+                .Create()
+                .WithCronSchedule("0 */2 * ? * * *")
+                .Build();
+
             scheduler.ScheduleJob(job1, every10s);
             scheduler.ScheduleJob(job2, every20s);
             scheduler.ScheduleJob(job2, every30s);
+            scheduler.ScheduleJob(job3, every2min);
 
             this.Scheduler = scheduler;
         }
